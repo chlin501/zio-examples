f9c9d71e5219013a12e34eeb3a88d95edd9ecc7d
Chia-Hung Lin <clin4j@googlemail.com>
2020-05-30 19:12:56 +0200

    Switch to manage zio.Queue resource

--- src/main/scala/examples/resource/App.scala
+++ src/main/scala/examples/resource/App.scala
@@ -5,11 +5,15 @@ import zio._
 object App {
 
   def main(args: Array[String]): Unit = {
+    val queue = Queue.bounded[Int](requestedCapacity = 32)
     val result = for {
-      listOfIntRef <- Ref.make[List[Int]](Nil)
-      res = (v: Int) => ZManaged.make(listOfIntRef.update(v :: _))(_ => listOfIntRef.update(v :: _))
+      q <- queue
+      // release is a fn checking the result of acquire
+      // if release do (_ => q.shutdown), it will throw zio.FiberFailure: Fiber failed. An interrupt was produced by #0.
+      release = (successOrNot: Boolean) => ZIO.unit
+      res = (v: Int) => ZManaged.make(q.offer(v))(release) 
       program = res(1) *> res(2) *> res(3)
-      values <- program.use_(ZIO.unit) *> listOfIntRef.get
+      values <- program.use_(ZIO.unit) *> q.take *> q.take *> q.take
     } yield values
     val runtime = Runtime.default
     println(runtime.unsafeRun(result))
